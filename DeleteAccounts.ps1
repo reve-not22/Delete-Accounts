8bba58159da896d732de91a7fd016e57ff8a91de
Damian <dematteodamian@gmail.com>
2023-05-22 13:45:28 -0400

    Initial Commit

---
+++ DeleteAccounts.ps1
@@ -0,0 +1,20 @@
+$excludeList = "admin", "cteadmin", "CTEAdmin", "Admin", "Damian D"
+
+$input = Read-Host 'Please input an account you would like to keep, or type N to ignore'
+if (($input -ne 'N') -or ($input -ne ' ')) {
+	$excludeList += $input
+}
+Get-ChildItem -Path C:\Users |
+
+Foreach-Object { 
+	$isInList = 0
+	foreach ($n in $excludeList) {
+		if ($_.Name -eq $n) {
+			$isInList = 1
+		}	
+	}
+	if ($isInList -ne 1) {
+		Write-Host "Deleting: " $_.Name
+		Remove-Item -LiteralPath $_.FullName -Force -Recurse
+	}
+}
